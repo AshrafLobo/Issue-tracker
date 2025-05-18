341396e12d2733f096393636bf207a559e965301
ashraflobo <ashraflobo@gmail.com>
2025-05-18 17:29:50 +0300

    Add new issues page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import NavBar from "./NavBar";
+import type { Metadata } from 'next';
+import './globals.css';
+import '@radix-ui/themes/styles.css';
+import { Theme } from '@radix-ui/themes';
+
+import NavBar from './NavBar';
 
 export const metadata: Metadata = {
-  title: "Issue Tracker App",
-  description: "Track issues and bugs in your projects",
+  title: 'Issue Tracker App',
+  description: 'Track issues and bugs in your projects',
 };
 
 export default function RootLayout({
@@ -15,8 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <NavBar />
-        <main>{children}</main>
+        <Theme>
+          <NavBar />
+          <main className="p-5">{children}</main>
+        </Theme>
       </body>
     </html>
   );
